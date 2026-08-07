705e4cb839c09eb8a8490087779aea891d368643
Ali Shafique <alishafique421@gmail.com>
2026-08-07 18:59:12 +0400

    Add Controls and other shit

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55757431,
+    wasmFileSize: 55812373,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
