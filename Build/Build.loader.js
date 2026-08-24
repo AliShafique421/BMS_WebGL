5c5dfea8f5e2db789510bb12b3113c127527a2c9
Ali Shafique <alishafique421@gmail.com>
2026-08-24 16:16:30 +0500

    update

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55812373,
+    wasmFileSize: 57949148,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
