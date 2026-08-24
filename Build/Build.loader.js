4fa73d848ae5db38e3521d73e7dd7252e8b4b342
Ali Shafique <alishafique421@gmail.com>
2026-08-24 17:24:13 +0500

    fix fucking black screen issue

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57949148,
+    wasmFileSize: 57948995,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
