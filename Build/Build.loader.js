7def45a5d1071e0239c1fce658da572fdab4fce9
Ali Shafique <alishafique421@gmail.com>
2026-08-25 14:24:39 +0500

    fix controls and hazard spawning issue

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57948995,
+    wasmFileSize: 57949049,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
